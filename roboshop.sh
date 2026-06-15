6e12d15389a9a534b8685cb6db82da78174b4b94
rakesh <rakeshgautam602@gmail.com>
2026-06-15 21:53:14 +0530

    committing roboshop

---
+++ roboshop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SG_ID="sg-0978bd3e9e6d67ee8"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+   INSTANCEID=$(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type t3.micro \
+        --security-group-ids $SG_ID \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceID' \
+        --output text)
+done
+
+if [ instance == frontend ]; then
+   IP=$(aws ec2 describe-instances \
+        --instance-ids $InstanceID \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
+        --output text)
+else
+   IP=$(aws ec2 describe-instances \
+        --instance-ids $InstanceID \
+        --query "Reservations[*].Instances[*].PrivateIpAddress" \
+        --output text)
+fi
+
+echo "IP Address: $IP"
