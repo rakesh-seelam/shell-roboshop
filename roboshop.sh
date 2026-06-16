208ebd0bc14db5ab023ea9b120fa962c9075547e
rakesh <rakeshgautam602@gmail.com>
2026-06-16 05:57:06 +0530

    committing roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -20,67 +20,9 @@ if [ instance == "frontend" ]; then
         --output text)
 else
    IP=$(aws ec2 describe-instances \
-        --instance-ids $INSTANCEID \
+        --instance-ids $InstanceID \
         --query "Reservations[].Instances[].PrivateIpAddress" \
         --output text)
 fi
    echo "IP Address: $IP"
 done
-
-for instance in $@
-do
-    INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type "t3.micro" \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text )
-
-#     if [ $instance == "frontend" ]; then
-#         IP=$(
-#             aws ec2 describe-instances \
-#             --instance-ids $INSTANCE_ID \
-#             --query 'Reservations[].Instances[].PublicIpAddress' \
-#             --output text
-#         )
-#         RECORD_NAME="$DOMAIN_NAME" # daws88s.online
-#     else
-#         IP=$(
-#             aws ec2 describe-instances \
-#             --instance-ids $INSTANCE_ID \
-#             --query 'Reservations[].Instances[].PrivateIpAddress' \
-#             --output text
-#         )
-#         RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
-#     fi
-
-#     echo "IP Address: $IP"
-
-#     aws route53 change-resource-record-sets \
-#     --hosted-zone-id $ZONE_ID \
-#     --change-batch '
-#     {
-#         "Comment": "Updating record",
-#         "Changes": [
-#             {
-#             "Action": "UPSERT",
-#             "ResourceRecordSet": {
-#                 "Name": "'$RECORD_NAME'",
-#                 "Type": "A",
-#                 "TTL": 1,
-#                 "ResourceRecords": [
-#                 {
-#                     "Value": "'$IP'"
-#                 }
-#                 ]
-#             }
-#             }
-#         ]
-#     }
-#     '
-
-#     echo "record updated for $instance"
-
-# done
-
