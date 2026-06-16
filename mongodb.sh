bdbbbac2b542dda53acf6a11b8006beb3b542f16
rakesh <rakeshgautam602@gmail.com>
2026-06-16 11:38:19 +0530

    committing mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -24,13 +24,13 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying mongo Repo"
 
-dnf install mongodb-org -y | &>>$LOG_FILE
+dnf install mongodb-org -y  &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB"
 
-systemctl enable mongod | &>>$LOG_FILE
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling Mongodb"
 
-systemctl start mongodb
+systemctl start mongod
 VALIDATE $? "Starting Mongodb"
 
 sed -i s/127.0.0.1/0.0.0.0/g /etc/mongod.conf
