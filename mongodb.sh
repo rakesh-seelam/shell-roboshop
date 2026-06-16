e0ded59348257bdb3d70b51f1056993ce04bbb44
rakesh <rakeshgautam602@gmail.com>
2026-06-16 08:21:58 +0530

    committing mongodb

---
+++ mongodb.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+USER_ID=$(id -u)
+LOG_FOLDER="/etc/var/logs/shell-roboshop"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+
+if [ $USER_ID -ne 0 ]; then
+   echo "Run this script as root user" | tee -a LOG_FILE
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2: FAILURE"
+      exit 1
+    else
+      echo "$2: Success"
+    fi
+}
+
+cp mongdb.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y | &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod | &>>$LOG_FILE
+VALIDATE $? "Enabling Mongodb"
+
+systemctl start mongod 
+VALIDATE $? "Starting Mongodb"
+
+sed -i s/127.0.0.1/0.0.0.0/g /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
+
