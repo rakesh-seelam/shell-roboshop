2d0ec615948083781df2cd6b8fdad2306034d261
rakesh <rakeshgautam602@gmail.com>
2026-06-17 14:49:09 +0530

    committing msql

---
+++ rabbitmq.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOG_FOLDER="/var/log/shell-roboshop"
+LOG_FILE="$LOG_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+mkdir -p $LOG_FOLDER
+
+if [ $USER_ID -ne 0 ]; then
+   echo -e "$R Run this script as root user $N" | tee -a $LOG_FILE
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo -e "$2: $R FAILURE $N" | tee -a $LOG_FILE
+      exit 1
+    else
+      echo -e "$2: $G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added RabbitMQ repo"
+
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATE $? "Installing rabbitmq"
+
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl start rabbitmq-server
+VALIDATE $? "Enabling and Starting rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "created user and gien permissions"
