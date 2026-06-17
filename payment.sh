428f553d56a3c862df57df59d8ac58f929e7f408
rakesh <rakeshgautam602@gmail.com>
2026-06-17 15:32:33 +0530

    committing rabbitmq

---
+++ payment.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOG_FOLDER="/var/log/shell-roboshop"
+LOG_FILE="$LOG_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
+
+if [ $USER_ID -ne 0 ]; then
+   echo -e "$R Run this script as root user $N" | tee -a $LOG_FILE
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo -e "$2: $R FAILURE $N" | tee -a $LOG_FILE
+      exit 1
+    else
+      echo -e "$2: $G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install python3 gcc python3-devel -y
+VALIDATE $? "Installed Python"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+   VALIDATE $? "Adding User"
+else
+   echo -e "User already exists $Y SKIPPING $N"
+if
+
+mkdir /app 
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading Zip code"
+
+cd /app 
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Delecting existing code"
+
+unzip /tmp/payment.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping code"
+
+cd /app 
+pip3 install -r requirements.txt &>>$LOG_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable payment &>>$LOG_FILE
+systemctl start payment
+VALIDATE $? "Enabled and started payment"
